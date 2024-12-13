fc36918dc9570fcb6170aaf6f3de81f1c78060d2
dnday <marceldino9@gmail.com>
2024-12-14 00:31:43 +0700

    feat: change data

--- STATIS/main.py
+++ STATIS/main.py
@@ -2,26 +2,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Baca file xlsx
-df = pd.read_excel('STATIS.xlsx')
+df = pd.read_excel('STATIS2.xlsx')
 
-# Buat grafik teta x terhadap time
-plt.plot(df['time'], df['teta x'])
-plt._auto_draw_if_interactive
+# Buat grafik teta x, teta y, dan teta z terhadap time dalam satu gambar
+plt.figure(figsize=(10, 6))
+
+plt.plot(df['time'], df['teta x'], label='Teta x')
+plt.plot(df['time'], df['teta y'], label='Teta y')
+plt.plot(df['time'], df['teta z'], label='Teta z')
+
 plt.xlabel('Time')
-plt.ylabel('Teta x')
-plt.title('Grafik Teta x terhadap Time')
+plt.ylabel('Teta')
+plt.title('Grafik Teta x, Teta y, dan Teta z terhadap Time')
+plt.legend()
+
 plt.show()
 
-# Buat grafik teta y terhadap time
-plt.plot(df['time'], df['teta y'])
-plt.xlabel('Time')
-plt.ylabel('Teta y')
-plt.title('Grafik Teta y terhadap Time')
-plt.show()
+# Buat grafik αx, αy, dan αz terhadap time dalam satu gambar
+plt.figure(figsize=(10, 6))
 
-# Buat grafik teta z terhadap time
-plt.plot(df['time'], df['teta z'])
+plt.plot(df['time'], df['αx'], label='αx (rad/s^2)')
+plt.plot(df['time'], df['αy'], label='αy (rad/s^2)')
+plt.plot(df['time'], df['αz'], label='αz (rad/s^2)')
+
 plt.xlabel('Time')
-plt.ylabel('Teta z')
-plt.title('Grafik Teta z terhadap Time')
+plt.ylabel('α')
+plt.title('Grafik αx, αy, dan αz terhadap Time')
+plt.legend()
+
 plt.show()
